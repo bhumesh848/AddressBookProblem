0605c640a653241bfdaffd34941619ddf7fa8e65
Mac02 <mac02@Mac02s-MacBook-Pro.local>
2024-12-02 11:35:33 +0530

    refactor the code

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,7 +1,8 @@
 package com.bridgelabz;
 
 public class AddressBookMain {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Welcome to Address book program ");
     }
 }
